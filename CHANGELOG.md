4400d52783283214e54470f10e60d0a6115715ae
Jorge Aparicio <japaricious@gmail.com>
2017-01-22 21:54:36 -0500

    tweak the signature and add documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,14 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
 
+### Changed
+
+- [breaking-change] The signature of the `zero_bss` and `init_data` functions;
+  the end `.bss`/`.data` pointer now must be `*mut T`. This makes it impossible
+  for people to bind a `static`, i.e. not `static mut`, variable to the end
+  of these sections, which could make the compiler, or other programmer, think
+  that such variable is allocated in the `.rodata` or the `.text` section.
+
 ## v0.1.0 - 2016-10-03
 
 ### Added
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,50 @@
 //! Initialization code ("crt0") written in Rust
+//!
+//! This is for bare metal systems where there is no ELF loader or OS to take
+//! care of initializing RAM for the program.
+//!
+//! # Example
+//!
+//! On the linker script side, we must assign names (symbols) to the boundaries
+//! of the `.bss` and `.data` sections.
+//!
+//! ``` text
+//! .bss : ALIGN(4)
+//! {
+//!     _sbss = .;
+//!     *(.bss.*);
+//!     _ebss = ALIGN(4);
+//! } > RAM
+//!
+//! .data : ALIGN(4)
+//! {
+//!     _sdata = .;
+//!     *(.data.*);
+//!     _edata = ALIGN(4);
+//! } > RAM AT > FLASH
+//!
+//! _sidata = LOADADDR(.data);
+//! ```
+//!
+//! On the Rust side, we must bind to those symbols using an `extern` block.
+//!
+//! ```
+//! unsafe fn before_main() {
+//!     // The type, `u32`, indicates that the memory is 4-byte aligned
+//!     extern "C" {
+//!         static mut _sbss: u32;
+//!         static mut _ebss: u32;
+//!
+//!         static mut _sdata: u32;
+//!         static mut _edata: u32;
+//!
+//!         static _sidata: u32;
+//!     }
+//!
+//!     zero_bss(&mut _sbss, &mut _ebss);
+//!     init_data(&mut _sdata, &mut _edata, &_sidata);
+//! }
+//! ```
 
 #![deny(warnings)]
 #![no_std]
@@ -10,7 +56,8 @@ use core::{mem, ptr};
 /// # Arguments
 ///
 /// - `sdata`. Pointer to the start of the `.data` section.
-/// - `edata`. Pointer to the end of the `.data` section.
+/// - `edata`. Pointer to the open/non-inclusive end of the `.data` section.
+///   (The value behind this pointer will not be modified)
 /// - `sidata`. `.data` section Load Memory Address (LMA)
 /// - Use `T` to indicate the alignment of the `.data` section and its LMA.
 ///
@@ -19,9 +66,10 @@ use core::{mem, ptr};
 /// - Must be called exactly once
 /// - `mem::size_of::<T>()` must be non-zero
 /// - `edata >= sdata`
-/// - The `sdata -> edata` region must not overlap with the `sidata -> ...` region
+/// - The `sdata -> edata` region must not overlap with the `sidata -> ...`
+///   region
 /// - `sdata`, `edata` and `sidata` must be `T` aligned.
-pub unsafe fn init_data<T>(sdata: *mut T, edata: *const T, sidata: *const T)
+pub unsafe fn init_data<T>(sdata: *mut T, edata: *mut T, sidata: *const T)
     where T: Copy
 {
     let n = (edata as usize - sdata as usize) / mem::size_of::<T>();
@@ -34,7 +82,8 @@ pub unsafe fn init_data<T>(sdata: *mut T, edata: *const T, sidata: *const T)
 /// # Arguments
 ///
 /// - `sbss`. Pointer to the start of the `.bss` section.
-/// - `ebss`. Pointer to the end of the `.bss` section.
+/// - `ebss`. Pointer to the open/non-inclusive end of the `.bss` section.
+///   (The value behind this pointer will not be modified)
 /// - Use `T` to indicate the alignment of the `.bss` section.
 ///
 /// # Safety
@@ -43,7 +92,7 @@ pub unsafe fn init_data<T>(sdata: *mut T, edata: *const T, sidata: *const T)
 /// - `mem::size_of::<T>()` must be non-zero
 /// - `ebss >= sbss`
 /// - `sbss` and `ebss` must be `T` aligned.
-pub unsafe fn zero_bss<T>(sbss: *mut T, ebss: *const T)
+pub unsafe fn zero_bss<T>(sbss: *mut T, ebss: *mut T)
     where T: Copy
 {
     let n = (ebss as usize - sbss as usize) / mem::size_of::<T>();
